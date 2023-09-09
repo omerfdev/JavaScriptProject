dd6355d996499cbbbec241c185a2d5f04726393b
omerfdev <omerfdev@gmail.com>
2023-09-09 23:27:27 +0300

    Quiz App Commit

--- JavaScriptProject/wwwroot/js/quizApp.js
+++ JavaScriptProject/wwwroot/js/quizApp.js
@@ -32,21 +32,59 @@
 		correct: 'b'
 	}
 ]
-
+const answersEls = document.querySelectorAll(".answer");
 const question = document.getElementById("question");
+const quiz = document.getElementById("quiz");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
 const c_text = document.getElementById("c_text");
 const d_text = document.getElementById("d_text");
+const submit = document.getElementById('submit');
 
 let currentQuiz = 0;
+let answer = undefined;
+let score = 0;
 loadQuiz();
+
 function loadQuiz() {
+	deselectAnswers();	
 	const currentQuizData = quizData[currentQuiz];
 	question.innerText = currentQuizData.question;
 	a_text.innerText = currentQuizData.a;
 	b_text.innerText = currentQuizData.b;
 	c_text.innerText = currentQuizData.c;
 	d_text.innerText = currentQuizData.d;
-	currentQuiz++;
 }
+
+function getAnswer() {
+	
+	let answer = undefined;
+	answersEls.forEach((answerEl) => {
+		if (answerEl.checked) {
+			return answer = answerEl.id;
+		}
+	});
+	return answer;
+}
+
+function deselectAnswers() {
+	answersEls.forEach((answerEl) => {
+		answerEl.checked = false;
+	});
+}
+
+submit.addEventListener('click', () => {
+	const answer = getAnswer();
+	if (answer) {
+		if (answer === quizData[currentQuiz].correct) {
+			score++;
+			currentQuiz++;
+			currentQuiz < quizData.length ? loadQuiz() : quiz.innerHTML = `<h2>Correct answer:${score}/${quizData.length}</h2><button onclick="location.reload()">Reload</button>`;
+		}
+		else { currentQuiz++; currentQuiz < quizData.length ? loadQuiz() : quiz.innerHTML = `<h2>Correct answer:${score}/${quizData.length}</h2><button onclick="location.reload()">Reload</button>`; }
+	}
+	else {
+		alert("Choose answer");
+	}
+
+});
