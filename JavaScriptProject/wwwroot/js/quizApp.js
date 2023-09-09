736941bec0d87eee2e2f33033fe1a632d049f993
omerfdev <omerfdev@gmail.com>
2023-09-09 16:58:21 +0300

    Bug

--- JavaScriptProject/wwwroot/js/quizApp.js
+++ JavaScriptProject/wwwroot/js/quizApp.js
@@ -1,45 +1,43 @@
 ﻿const quizData = [
-    {
-        question: 'Best Football Manager SEGA game?',
-        a: '2012',
-        b: '2005',
-        c: '2023',
-        d: '2007',
-        correct: 'b',
-    },
-    {
-        question: 'Best programming language?',
-        a: 'Go',
-        b: 'C++',
-        c: 'Java',
-        d: 'All',
-        correct: 'd',
-    },
-    {
-        question: 'Best sci-fi Film?',
-        a: 'Harry Potter',
-        b: 'Lord Of the Rings',
-        c: 'Breaking Bad',
-        d: 'None of the above',
-        correct: 'a',
-    },
-    {
-        question: 'Best Entrepreneur?',
-        a: 'Elon Musk',
-        b: 'Nikola Tesla',
-        c: 'Mark Zuckerberg',
-        d: 'Jeff Bezos',
-        correct: 'b',
-    }
-];
+	{
+		question: 'Best Football Manager SEGA game?',
+		a: '2012',
+		b: '2005',
+		c: '2023',
+		d: '2007',
+		correct: 'b'
+	},
+	{
+		question: 'Best programming language?',
+		a: 'Go',
+		b: 'C++',
+		c: 'Java',
+		d: 'All',
+		correct: 'd'
+	},
+	{
+		question: 'Best sci-fi Film?',
+		a: 'Harry Potter',
+		b: 'Lord Of the Rings',
+		c: 'Breaking Bad',
+		d: 'None of the above',
+		correct: 'a'
+	},
+	{
+		question: 'Best Entrepreneur?',
+		a: 'Elon Musk',
+		b: 'Nikola Tesla',
+		c: 'Mark Zuckerberg',
+		d: 'Jeff Bezos',
+		correct: 'b'
+	}
+]
 
-
-
-const question = document.getElementById('question');
-const a_text = document.getElementById('a_text');
-const b_text = document.getElementById('b_text');
-const c_text = document.getElementById('c_text');
-const d_text = document.getElementById('d_text');
+const question = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
 
 let currentQuiz = 0;
 loadQuiz();
@@ -50,4 +48,5 @@ function loadQuiz() {
 	b_text.innerText = currentQuizData.b;
 	c_text.innerText = currentQuizData.c;
 	d_text.innerText = currentQuizData.d;
+	currentQuiz++;
 }
