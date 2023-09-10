f87ac8ed77b4213b29ed6d1ac2efb85db77450fc
omerfdev <omerfdev@gmail.com>
2023-09-10 21:00:23 +0300

    Commit

--- JavaScriptProject/Controllers/HomeController.cs
+++ JavaScriptProject/Controllers/HomeController.cs
@@ -1,35 +1,70 @@
-﻿using JavaScriptProject.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using System;
 using System.Diagnostics;
+using System.Net.Http;
+using System.Threading.Tasks;
+using JavaScriptProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace JavaScriptProject.Controllers
 {
-	public class HomeController : Controller
-	{
-		private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly string apiUrl = "https://www.themealdb.com/api/json/v1/1/categories.php";
 
-		public HomeController(ILogger<HomeController> logger)
-		{
-			_logger = logger;
-		}
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-		public IActionResult Index()
-		{
-			return View();
-		}
+        public IActionResult Index()
+        {
+            return View();
+        }
+
         public IActionResult QuizApp()
         {
             return View();
         }
-        public IActionResult Privacy()
-		{
-			return View();
-		}
 
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-		public IActionResult Error()
-		{
-			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-		}
-	}
+        public async Task<IActionResult> RecipeApp()
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var content = await response.Content.ReadAsStringAsync();
+                        var categories = JsonConvert.DeserializeObject<CategoryModel>(content);
+                        ViewBag.Categories = categories;
+                        return View("recipeApp", categories);
+                    }
+                    else
+                    {
+                        return View("Error");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View("Error");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
 }
---
+++ JavaScriptProject/Models/CategoryModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace JavaScriptProject.Models
+{
+    public class CategoryModel:IEnumerator
+    {
+        public int idCategory { get; set; }
+        public string strCategory { get; set; }
+        public string strCategoryThumb { get; set; }
+        public string strCategoryDescription { get; set; }
+        public List<CategoryModel>? Categories { get; set; }
+
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
